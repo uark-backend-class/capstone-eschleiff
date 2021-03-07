97f883f516769f2f3755f3c1f3ba08a86a5cc655
Eric Schleiff <ericschleiff@gmail.com>
2021-03-06 22:10:17 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const passport = require('passport');
+const { initialize } = require('passport');
+
+//Create express app
+const app = express(); 
+
+//This is the folder that has the hbs or pug  files
+app.set('views', path.join(__dirname, 'views'));
+
+//Sets pug as my viewing engine
+app.set('view engine', 'pug');
+
+//
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
+
+module.exports = app;

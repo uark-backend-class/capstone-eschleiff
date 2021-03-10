6a7353d6d80feebb70a395ddc8e473bbd5577602
Schleiff <DCH746@zebra.com>
2021-03-10 15:06:27 -0600

    updates 3/10

--- launches.api.js
+++ launches.api.js
@@ -2,10 +2,15 @@ const axios = require('axios');
 const baseUrl = 'https://api.spacexdata.com/v4/'
 
 let launchData;
+let launchNameData = [];
 
 async function getAllLaunches() {
     const response = await axios.get(baseUrl + 'launches');
     launchData = JSON.stringify(response.data);
+
+   for (names in response) {
+       console.log(names);
+   }
 
     return launchData;
 
@@ -14,3 +19,5 @@ async function getAllLaunches() {
 module.exports = {
     getAllLaunches
 };
+
+getAllLaunches();

dc42b1ec2360b96afd2b9826763928a4fdd18bbc
Eric Schleiff <ericschleiff@gmail.com>
2021-03-28 18:40:44 -0500

    working on flashes

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ const flash = require('connect-flash');
 //Create express app
 const app = express(); 
 
+// Static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// populate req.cookies
+app.use(cookieParser());
+
+// keeps users logged into the session
+app.use(session({ 
+    secret: 'doge',
+    key: 'sesh',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// tells app to use our flash messages
+app.use(flash());
+
+// Tell app to use passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //This is the folder that has the hbs or pug  files
 app.set('views', path.join(__dirname, 'views'));
 
@@ -22,8 +47,6 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-// populate req.cookies
-app.use(cookieParser());
 
 // Pare url encoded data and put it into req.body
 app.use(express.urlencoded({extended: true}));
@@ -32,27 +55,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Parse JSON data and put it into req.body
 app.use(express.json());
-
-// keeps users logged into the session
-app.use(session({ 
-    secret: 'doge',
-    resave: false,
-    saveUninitialized: false,
-}));
-
-// Tell app to use passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Static authenticate method of model in LocalStrategy
-passport.use(User.createStrategy());
-
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// tells app to use our flash messages
-app.use(flash());
 
 // tells our app what routes to use
 app.use(routes);
